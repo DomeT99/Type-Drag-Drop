cd76ba42b6146eb4472dfda090b31225564c4948
DomeT99 <domenicotenace99@gmail.com>
2022-08-30 17:45:46 +0200

    Validazione form

--- src/app.js
+++ src/app.js
@@ -6,6 +6,32 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
 /**
+ * If the input is required, it must have a value. If the input has a minLength, it must be at least
+ * that long. If the input has a maxLength, it must be at most that long. If the input has a min, it
+ * must be at least that value. If the input has a max, it must be at most that value.
+ * @param {Validatable} validatableInput - Validatable
+ * @returns The function validate is being returned.
+ */
+function validate(validatableInput) {
+    var isValid = true;
+    if (validatableInput.required) {
+        isValid = isValid && validatableInput.value.toString().trim().length !== 0;
+    }
+    if (validatableInput.minLength != null && typeof validatableInput.value === "string") {
+        isValid = isValid && validatableInput.value.length >= validatableInput.minLength;
+    }
+    if (validatableInput.maxLength != null && typeof validatableInput.value === "string") {
+        isValid = isValid && validatableInput.value.length <= validatableInput.maxLength;
+    }
+    if (validatableInput.min != null && typeof validatableInput.value === "number") {
+        isValid = isValid && validatableInput.value >= validatableInput.min;
+    }
+    if (validatableInput.max != null && typeof validatableInput.value === "number") {
+        isValid = isValid && validatableInput.value <= validatableInput.max;
+    }
+    return isValid;
+}
+/**
  * "If the descriptor is not undefined, then create a new descriptor that returns a bound function of
  * the original method."
  *
@@ -60,10 +86,65 @@ var ProjectInput = /** @class */ (function () {
         /* Calling the attach method, passing the element property as an argument. */
         this.attach(this.element);
     }
+    /**
+     * It takes the values from the input fields, checks if they are valid, and if they are, it returns
+     * them as an array.
+     *
+     * If they are not valid, it alerts the user and returns nothing.
+     *
+     * The function is called in the following way:
+     *
+     * const userInput = this.gatherUserInput();
+     *
+     * if (Array.isArray(userInput)) {
+     *     const [title, desc, people] = userInput;
+     *     console.log(title, desc, people);
+     * }
+     * @returns an array of strings and numbers.
+     */
+    ProjectInput.prototype.gatherUserInput = function () {
+        var enteredTitle = this.titleInputElement.value;
+        var enteredDescription = this.descriptionInputElement.value;
+        var enteredPeople = this.peopleInputElement.value;
+        var titleValidatable = {
+            value: enteredTitle,
+            required: true
+        };
+        var descriptionValidatable = {
+            value: enteredDescription,
+            required: true,
+            minLength: 5
+        };
+        var peopleValidatable = {
+            value: +enteredPeople,
+            required: true,
+            min: 1,
+            max: 5
+        };
+        if (!validate(titleValidatable) || !validate(descriptionValidatable) || !validate(peopleValidatable)) {
+            alert("Insert input, please try again!");
+        }
+        else {
+            return [enteredTitle, enteredDescription, +enteredPeople];
+        }
+    };
     /* A decorator that binds the submitHandler method to the class instance. */
     ProjectInput.prototype.submitHandler = function (event) {
         event.preventDefault();
-        console.log(this.titleInputElement.value);
+        var userInput = this.gatherUserInput();
+        if (Array.isArray(userInput)) {
+            var title = userInput[0], desc = userInput[1], people = userInput[2];
+            console.log(title, desc, people);
+            this.clearInput();
+        }
+    };
+    /**
+     * The function clears the input fields by setting the value of each input field to an empty string.
+     */
+    ProjectInput.prototype.clearInput = function () {
+        this.titleInputElement.value = '';
+        this.descriptionInputElement.value = '';
+        this.peopleInputElement.value = '';
     };
     /**
      * The configure function adds an event listener to the form element, and when the form is
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,46 @@
+/* An interface that is used to validate the input. */
+interface Validatable {
+    value: string | number;
+    required?: boolean;
+    minLength?: number;
+    maxLength?: number;
+    min?: number;
+    max?: number;
+}
+
+/**
+ * If the input is required, it must have a value. If the input has a minLength, it must be at least
+ * that long. If the input has a maxLength, it must be at most that long. If the input has a min, it
+ * must be at least that value. If the input has a max, it must be at most that value.
+ * @param {Validatable} validatableInput - Validatable
+ * @returns The function validate is being returned.
+ */
+function validate(validatableInput: Validatable) {
+    let isValid = true;
+
+    if (validatableInput.required) {
+        isValid = isValid && validatableInput.value.toString().trim().length !== 0;
+    }
+
+    if (validatableInput.minLength != null && typeof validatableInput.value === "string") {
+        isValid = isValid && validatableInput.value.length >= validatableInput.minLength;
+    }
+
+    if (validatableInput.maxLength != null && typeof validatableInput.value === "string") {
+        isValid = isValid && validatableInput.value.length <= validatableInput.maxLength;
+    }
+
+    if (validatableInput.min != null && typeof validatableInput.value === "number") {
+        isValid = isValid && validatableInput.value >= validatableInput.min;
+    }
+
+    if (validatableInput.max != null && typeof validatableInput.value === "number") {
+        isValid = isValid && validatableInput.value <= validatableInput.max;
+    }
+
+    return isValid;
+}
+
 /**
  * "If the descriptor is not undefined, then create a new descriptor that returns a bound function of
  * the original method."
@@ -67,13 +110,73 @@ class ProjectInput {
         this.attach(this.element);
     }
 
+    /**
+     * It takes the values from the input fields, checks if they are valid, and if they are, it returns
+     * them as an array.
+     * 
+     * If they are not valid, it alerts the user and returns nothing.
+     * 
+     * The function is called in the following way:
+     * 
+     * const userInput = this.gatherUserInput();
+     * 
+     * if (Array.isArray(userInput)) {
+     *     const [title, desc, people] = userInput;
+     *     console.log(title, desc, people);
+     * }
+     * @returns an array of strings and numbers.
+     */
+    private gatherUserInput(): [string, string, number] | void {
+        const enteredTitle = this.titleInputElement.value;
+        const enteredDescription = this.descriptionInputElement.value;
+        const enteredPeople = this.peopleInputElement.value;
+
+        const titleValidatable: Validatable = {
+            value: enteredTitle,
+            required: true
+        }
+
+        const descriptionValidatable: Validatable = {
+            value: enteredDescription,
+            required: true,
+            minLength: 5
+        }
+
+        const peopleValidatable: Validatable = {
+            value: +enteredPeople,
+            required: true,
+            min: 1,
+            max: 5
+        }
+
+        if (!validate(titleValidatable) || !validate(descriptionValidatable) || !validate(peopleValidatable)) {
+            alert("Insert input, please try again!")
+        } else {
+            return [enteredTitle, enteredDescription, +enteredPeople]
+        }
+    }
+
     /* A decorator that binds the submitHandler method to the class instance. */
     @Autobind
     private submitHandler(event: Event) {
         event.preventDefault();
-        console.log(this.titleInputElement.value);
+        const userInput = this.gatherUserInput();
+
+        if (Array.isArray(userInput)) {
+            const [title, desc, people] = userInput;
+            console.log(title, desc, people)
+            this.clearInput()
+        }
     }
 
+    /**
+     * The function clears the input fields by setting the value of each input field to an empty string.
+     */
+    private clearInput() {
+        this.titleInputElement.value = '';
+        this.descriptionInputElement.value = '';
+        this.peopleInputElement.value = '';
+    }
 
     /**
      * The configure function adds an event listener to the form element, and when the form is
