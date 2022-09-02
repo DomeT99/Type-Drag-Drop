9c6c7097a6c2e2941ae2d50ffaf44b008d69cd6f
DomeT99 <domenicotenace99@gmail.com>
2022-09-02 13:49:34 +0200

    Funzione get in ProjectItem

--- src/app.js
+++ src/app.js
@@ -281,10 +281,22 @@ var ProjectItem = /** @class */ (function (_super) {
         _this.renderContent();
         return _this;
     }
+    Object.defineProperty(ProjectItem.prototype, "persons", {
+        get: function () {
+            if (this.project.people === 1) {
+                return '1 person';
+            }
+            else {
+                return "".concat(this.project.people, " persons");
+            }
+        },
+        enumerable: false,
+        configurable: true
+    });
     ProjectItem.prototype.configure = function () { };
     ProjectItem.prototype.renderContent = function () {
         this.element.querySelector("h2").textContent = this.project.title;
-        this.element.querySelector("h3").textContent = this.project.people.toString();
+        this.element.querySelector("h3").textContent = this.persons + ' assigned';
         this.element.querySelector("p").textContent = this.project.description;
     };
     return ProjectItem;
--- src/app.ts
+++ src/app.ts
@@ -280,6 +280,14 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> {
 class ProjectItem extends Component<HTMLUListElement, HTMLLIElement>{
     private project: Project
 
+    get persons() {
+        if (this.project.people === 1) {
+            return '1 person'
+        } else {
+            return `${this.project.people} persons`
+        }
+    }
+
 
     constructor(hostId: string, project: Project) {
         super('single-project', hostId, false, project.id);
@@ -292,7 +300,7 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement>{
     configure(): void { }
     renderContent() {
         this.element.querySelector("h2")!.textContent = this.project.title;
-        this.element.querySelector("h3")!.textContent = this.project.people.toString();
+        this.element.querySelector("h3")!.textContent = this.persons + ' assigned';
         this.element.querySelector("p")!.textContent = this.project.description;
     }
 }
